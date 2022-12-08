9f58a9b6966f3150ed0fe68c865355af7b3f5dc1
Bhinney <snakellie97@gmail.com>
2022-12-08 22:16:13 +0900

    #3 feat : 필요한 entity class 생성

---
+++ kakaopay/src/main/java/payment/entity/Auditable.java
@@ -0,0 +1,29 @@
+package payment.entity;
+
+import java.time.LocalDate;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class Auditable {
+	/* 생성 시간 */
+	@CreatedDate
+	@Column(updatable = false)
+	private LocalDate createdAt;
+
+	/* 수정 시간 */
+	@LastModifiedDate
+	private LocalDate modifiedAt;
+}
