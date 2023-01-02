dcf92f4f1978fb33cf2ecb1d7e2f98c7b7b971de
Bhinney <snakellie97@gmail.com>
2023-01-02 23:59:00 +0900

    #22 feat : entity 구현

---
+++ boardProject/src/main/java/org/study/boardProject/entity/Board.java
@@ -0,0 +1,47 @@
+package org.study.boardProject.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.study.boardProject.global.Auditable;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Builder
+@Getter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+public class Board extends Auditable {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long boardId;
+
+	@Column(length = 50, nullable = false)
+	private String nickName;
+
+	@Column(length = 100, nullable = false)
+	private String title;
+
+	@Column(columnDefinition = "TEXT", nullable = false)
+	private String content;
+
+	private void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+
+	private void setTitle(String title) {
+		this.title = title;
+	}
+
+	private void setContent(String content) {
+		this.content = content;
+	}
+}
