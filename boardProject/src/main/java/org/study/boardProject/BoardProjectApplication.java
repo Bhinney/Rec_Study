8e925640765b2663bfd114ac109891043d493394
Bhinney <snakellie97@gmail.com>
2023-01-02 23:54:35 +0900

    #20 setting : 초기 세팅

---
+++ boardProject/src/main/java/org/study/boardProject/BoardProjectApplication.java
@@ -0,0 +1,15 @@
+package org.study.boardProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class BoardProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoardProjectApplication.class, args);
+	}
+
+}
