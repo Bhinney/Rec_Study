71639c7f69fba46ab683977e861b4611ad1b0146
Bhinney <snakellie97@gmail.com>
2023-01-03 00:01:07 +0900

    #22 feat : service 틀 구현

---
+++ boardProject/src/main/java/org/study/boardProject/service/BoardService.java
@@ -0,0 +1,33 @@
+package org.study.boardProject.service;
+
+import org.springframework.stereotype.Service;
+import org.study.boardProject.entity.Board;
+
+@Service
+public class BoardService {
+
+	/* 게시판 등록 */
+	public Board create() {
+		return null;
+	}
+
+	/* 게시판 수정 */
+	public Board update() {
+		return null;
+	}
+
+	/* 특정 게시판 조회 */
+	public Board findBoard() {
+		return null;
+	}
+
+	/* 전체 게시판 조회 */
+	public Board findBoardList() {
+		return null;
+	}
+
+	/* 게시판 삭제 */
+	public Board delete() {
+		return null;
+	}
+}
