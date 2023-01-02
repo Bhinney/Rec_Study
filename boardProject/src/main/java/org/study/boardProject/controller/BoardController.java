6b7c34a4233744315b0c0ae6fdde7c2cf8e9a631
Bhinney <snakellie97@gmail.com>
2023-01-03 00:58:08 +0900

    #22 feat : controller 구현 1차

--- boardProject/src/main/java/org/study/boardProject/controller/BoardController.java
+++ boardProject/src/main/java/org/study/boardProject/controller/BoardController.java
@@ -2,36 +2,73 @@ package org.study.boardProject.controller;
 
 /* 게시판 Controller */
 
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.study.boardProject.dto.BoardDto;
+import org.study.boardProject.entity.Board;
+import org.study.boardProject.mapper.BoardMapper;
+import org.study.boardProject.service.BoardService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequestMapping("/api/boards")
+@RequiredArgsConstructor
 public class BoardController {
 
+	private final BoardService boardService;
+	private final BoardMapper mapper;
+
 	/* 게시판 등록 */
-	public ResponseEntity post() {
-		return null;
+	@PostMapping
+	public ResponseEntity post(@RequestBody BoardDto.Post requestBody) {
+		Board board = boardService.create(mapper.boardPostDtoToBoard(requestBody));
+
+		return new ResponseEntity<>(mapper.boardToBoardResponseDto(board), HttpStatus.CREATED);
 	}
 
 	/* 게시판 수정 */
-	public ResponseEntity patch() {
-		return null;
+	@PatchMapping("/{board_id}")
+	public ResponseEntity patch(@PathVariable long boardId, @RequestBody BoardDto.Patch requestBody) {
+		Board board = boardService.update(mapper.boardPatchDtoToBoard(requestBody), boardId);
+
+		return ResponseEntity.ok(mapper.boardToBoardResponseDto(board));
 	}
 
 	/* 특정 게시판 조회 */
-	public ResponseEntity getBoard() {
-		return null;
+	@GetMapping("/{board_id}")
+	public ResponseEntity getBoard(@PathVariable long boardId) {
+		Board board = boardService.findBoard(boardId);
+
+		return ResponseEntity.ok(mapper.boardToBoardResponseDto(board));
 	}
 
 	/* 전체 게시판 조회 */
-	public ResponseEntity getBoardList() {
-		return null;
+	@GetMapping
+	public ResponseEntity getBoardList(@RequestParam int page, @RequestParam int size) {
+		Page<Board> boardPage = boardService.findBoardList(page - 1, size);
+		List<Board> boardList = boardPage.getContent();
+
+		return ResponseEntity.ok(mapper.boardToBoardResponseDto(boardList));
 	}
 
 	/* 게시판 삭제 */
-	public ResponseEntity delete() {
-		return null;
+	@DeleteMapping("/{board_id}")
+	public ResponseEntity delete(@PathVariable long boardId) {
+		String message = boardService.delete(boardId);
+
+		return ResponseEntity.ok(message);
 	}
 }
