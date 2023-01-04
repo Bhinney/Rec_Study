b4b486007c105d734420b3fa16ef0ccfe2fdd333
Bhinney <snakellie97@gmail.com>
2023-01-04 22:02:51 +0900

    #24 refactor : test를 위한 코드 추가

--- boardProject/src/main/java/org/study/boardProject/dto/BoardDto.java
+++ boardProject/src/main/java/org/study/boardProject/dto/BoardDto.java
@@ -2,6 +2,7 @@ package org.study.boardProject.dto;
 
 import java.time.LocalDate;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 
@@ -9,6 +10,7 @@ public class BoardDto {
 
 	@Getter
 	@Builder
+	@AllArgsConstructor /* 테스트를 위해 추가 */
 	public static class Post {
 		private String nickName;
 		private String title;
@@ -17,6 +19,7 @@ public class BoardDto {
 
 	@Getter
 	@Builder
+	@AllArgsConstructor /* 테스트를 위해 추가 */
 	public static class Patch {
 		private String nickName;
 		private String title;
@@ -25,6 +28,7 @@ public class BoardDto {
 
 	@Getter
 	@Builder
+	@AllArgsConstructor /* 테스트를 위해 추가 */
 	public static class Response {
 		private long boardId;
 		private String nickName;
