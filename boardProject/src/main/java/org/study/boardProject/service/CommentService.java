5286a77f672d1c47dbaf087d5be56191dd750f1a
Bhinney <snakellie97@gmail.com>
2023-01-09 08:00:19 +0900

    #31 feat : service 틀 구현

---
+++ boardProject/src/main/java/org/study/boardProject/service/CommentService.java
@@ -0,0 +1,28 @@
+package org.study.boardProject.service;
+
+import org.springframework.stereotype.Service;
+import org.study.boardProject.entity.Comment;
+
+@Service
+public class CommentService {
+
+	/* 댓글 등록 */
+	public Comment postComment(){
+		return null;
+	}
+
+	/* 댓글 수정 */
+	public Comment patchComment(){
+		return null;
+	}
+
+	/* 댓글 조회 */
+	public Comment getCommentList(){
+		return null;
+	}
+
+	/* 댓글 삭제 */
+	public String deleteComment(){
+		return null;
+	}
+}
