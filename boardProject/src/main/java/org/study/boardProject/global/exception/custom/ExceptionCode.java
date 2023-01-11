092bb11897f889a08c0aea3a3faeadcb5a635b43
Bhinney <snakellie97@gmail.com>
2023-01-11 18:17:02 +0900

    #36 refactor : exception 리팩터링

--- boardProject/src/main/java/org/study/boardProject/global/exception/custom/ExceptionCode.java
+++ boardProject/src/main/java/org/study/boardProject/global/exception/custom/ExceptionCode.java
@@ -5,10 +5,11 @@ import lombok.Getter;
 public enum ExceptionCode {
 
 	/* 게시판 관련 예외 */
-	BOARD_NOT_FOUND(404, "게시판을 찾을 수 없습니다."),
+	BOARD_NOT_FOUND(404, "게시판이 존재하지 않습니다."),
 
 	/* 댓글 관련 예외*/
-	COMMENT_NOT_FOUND(404, "댓글을 찾을 수 없습니다.");
+	COMMENT_NOT_FOUND(404, "댓글이 존재하지 않습니다."),
+	NOT_CORRECT_BOARDID(400, "게시판 정보가 댓글의 게시판 정보와 일치하지 않습니다.");
 
 
 	@Getter
